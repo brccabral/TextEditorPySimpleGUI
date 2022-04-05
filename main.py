62aaece7d62b81e9b615f85684af8cb9a35be796
Bruno Cabral <cabral.brc@gmail.com>
2022-04-05 16:19:11 -0700

    menu setup

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import PySimpleGUI as sg
 
+smileys = ["happy", [":)", "xD", ":D", "<3"], "sad", [":(", "T_T"], "other", [":3"]]
+smileys_events = smileys[1] + smileys[3] + smileys[5]
 
-layout = [[]]
+menu_layout = [
+    ["File", ["Open", "Save", "---", "Exit"]],
+    ["Tools", ["Word Count"]],
+    ["Add", smileys],
+]
+
+layout = [
+    [sg.Menu(menu_layout)],
+    [sg.Text("Untitled", key="-DOCNAME-")],
+    [sg.Multiline()],
+]
 
 window = sg.Window("Title", layout, size=(300, 300))
 
@@ -9,5 +21,8 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+
+    if event == "Word Count":
+        print("test")
 
 window.close()
